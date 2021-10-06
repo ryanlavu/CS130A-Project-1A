2897ef7b60493551a869ca5c0a5157ec74b06238
ryanlavu <ryanvu@ucsb.edu>
2021-10-05 20:57:14 -0700

    added reading code to the main

--- dictionary.cpp
+++ dictionary.cpp
@@ -1,4 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
 using namespace std;
+
+int main(int argc, char* argv[])
+{
+	ifstream wordBase;
+
+	string fileName;
+	vector<string> wordVector;
+	vecotr<int> intVector;
+	string word;
+	bool trueOrFalse = true;
+	fileName += argv[1];
+
+	wordBase.open(fileName);
+	if(wordBase >> word)
+	{
+		intVecotr.push_back(stringToInt(word));
+		wordVector.push_back(word);
+
+	}
+	while(wordBase >> word)
+	{
+		intVecotr.push_back(stringToInt(word));
+		for(int i = 0; i < wordVector.size(); i++)
+		{
+			if(wordVector[i] == word)
+				trueOrFalse = false;
+		}
+		if(trueOrFalse)
+			wordVector.push_back(word);
+		trueOrFalse = true;
+	}
+
+	wordBase.close();
+}
 
 int stringToInt(String input) {
 	

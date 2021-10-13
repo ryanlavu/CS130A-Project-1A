c89f881b19cd2167dc59050a35e6d683fc6cf707
Matthew Tran <matthewgtran@umail.ucsb.edu>
2021-10-12 21:08:22 -0700

    plz

--- dictionary.cpp
+++ dictionary.cpp
@@ -24,27 +24,6 @@ int stringToInt(string input) {
 
 }
 
-void insert(string data, int intArray[]) {
-
-	int index = stringToInt(data);
-
-	if(table[index]) {
-		
-		for(int i = 0; i < intArray[index]; i++) {
-
-			if(table[index][i] == "") {
-
-				table[index][i] = data;
-				return;
-
-			}
-
-		}
-
-	}
-
-}
-
 string indexOfQuery(string query, int intArray[]) {
 
 	int index = stringToInt(query);
@@ -127,18 +106,17 @@ int getLargestSlotIndex(int intArray[], int arraySize) {
 
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 
 	//To read the file containing p and c values
 	string getP, getC;
 	ifstream pAndC(argv[1]);
 	getline(pAndC,getP);
 	p = stoi(getP);
-	cout << "Printing p: " << p << endl;
+	//cout << "Printing p: " << p << endl;
 	getline(pAndC,getC);
 	c = stoi(getC);
-	cout << "Printing c: " << c << endl;
+	//cout << "Printing c: " << c << endl;
 
 	pAndC.close();
 
@@ -165,8 +143,8 @@ int main(int argc, char *argv[])
 		wordVector.push_back(word);
 		totalWords++;
 		uniqueWords++;
-		cout << word  << " if statement" << endl;
-		cout << intArray[0] << " if statement first line int" << endl;
+		//cout << word  << " if statement" << endl;
+		//cout << intArray[0] << " if statement first line int" << endl;
 	}
 	while(wordBase)
 	{
@@ -194,6 +172,86 @@ int main(int argc, char *argv[])
 
 	wordBase.close();
 
+	//Initializes the string arrays within the hashtable to the right size and if it is zero it is null
+	cout << "initialize the string arrays" << endl;
+	for(int i = 0; i < p; i++) {
+
+		if(intArray[i] > 0) {
+
+			hashTable[i] = new string[intArray[i]];
+
+		}
+
+	}
+	
+	//Inputs all the strings from wordVector into the hashtable
+	cout << "strings from wordVector into hashTable" << endl;
+	for(int i = 0; i < wordVector.size(); i++) {
+	
+		int index = stringToInt(wordVector[i]);
+
+		for(int i = 0; i < intArray[index]; i++) {
+
+			if(hashTable[index][i] == "") {
+
+				table[index][i] = wordVector[i];
+
+			}
+
+		}
+
+	}
+
+	//Print size of input, number of words, primary array size
+	cout << "Size of input: " << totalWords << endl;
+	cout << "Number of words in table: " << uniqueWords << endl;
+	cout << "Primary array size: " << p << endl;
+
+	//Find max number of collisions in a hashbucket
+	int max = intArray[0];
+	int maxIndex = 0;
+
+	for(int i = 1; i < p; i++) {
+
+		if(intArray[i] > max) {
+
+			max = intArray[i];
+			maxIndex = i;
+
+		}
+
+	}
+
+	cout << "Maximum number of collisions in a hashbucket: " << max << endl;
+	
+	//Find amount of arrays with sizes zero through twenty and print
+	int sizeArray[21] = {0};
+	for(int i = 0; i < p; i++) {
+	
+		if(intArray[i] > 20) {
+
+			sizeArray[intArray[i]]++;
+
+		}
+
+	}
+
+	cout << "The number of hashbuckets b with x elements:" << endl;
+
+	for(int i = 0; i < 21; i++) {
+
+		cout << "x=" << i << " : b=" << sizeArray[i] << endl;
+
+	}
+
+	//Print keys in the most populated bucket
+	cout << "Keys in most populated bucket:";
+
+	for(int i = 0; i < intArray[maxIndex]; i++) {
+
+		cout << " " << hashTable[maxIndex][i] << endl;
+
+	}
 
 	//checks if user inputs a query file
 	if(argc > 3)
